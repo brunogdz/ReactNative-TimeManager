f4be960262a4c06de02ec0c98cbe6d9abbf99fd8
Bruno Gomes <brunog.vix@gmail.com>
2021-06-25 02:28:34 -0300

    adicionado novas imagens

--- App.tsx
+++ App.tsx
@@ -4,11 +4,14 @@ import { StyleSheet, Text, View } from 'react-native';
 // import { UserIdentification } from './src/pages/UserIdentification';
 import Routes from './src/routes/index';
 import AppLoading from 'expo-app-loading';
+import { firebaseConfig } from './config';
+firebase.initializeApp(firebaseConfig)
 import {
   useFonts,
   Jost_400Regular,
   Jost_600SemiBold
 } from '@expo-google-fonts/jost';
+import { firebase } from '@react-native-firebase/auth';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
